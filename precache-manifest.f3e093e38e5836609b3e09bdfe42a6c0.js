9be2e35a799919e70701430158c0f7f49ab250d2
AlexGaard <AlexGaard@users.noreply.github.com>
2020-02-28 08:56:48 +0000

    Automated deployment: Fri Feb 28 08:56:48 UTC 2020 f8bb3bd3910482e047d39ef3031c5877364ca3e0

---
+++ precache-manifest.f3e093e38e5836609b3e09bdfe42a6c0.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec6510835ae5a7d37d93dc065e39d99",
+    "url": "/beslutteroversikt/index.html"
+  },
+  {
+    "revision": "105a067044e44fba62c8",
+    "url": "/beslutteroversikt/static/css/main.css"
+  },
+  {
+    "revision": "105a067044e44fba62c8",
+    "url": "/beslutteroversikt/static/js/main.js"
+  }
+]);
