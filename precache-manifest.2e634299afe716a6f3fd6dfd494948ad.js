74410a00fee28c9510a770efbd04e547dcbd8a2e
matiasvinjevoll <matiasvinjevoll@users.noreply.github.com>
2020-04-02 14:20:58 +0000

    Automated deployment: Thu Apr  2 14:20:58 UTC 2020 e2e68d688f2873becfe09674e89141c6b8352c00

---
+++ precache-manifest.2e634299afe716a6f3fd6dfd494948ad.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "087cadf55afef1ae092d56e64de8064c",
+    "url": "/beslutteroversikt/index.html"
+  },
+  {
+    "revision": "ccf919865279e6b2fe80",
+    "url": "/beslutteroversikt/static/css/main.css"
+  },
+  {
+    "revision": "ccf919865279e6b2fe80",
+    "url": "/beslutteroversikt/static/js/main.js"
+  },
+  {
+    "revision": "97d2dfd121ed155744fb050bd5f40581",
+    "url": "/beslutteroversikt/static/media/arrow-down.97d2dfd1.svg"
+  },
+  {
+    "revision": "b6d6d8c5f9d2184c6495969b4997763a",
+    "url": "/beslutteroversikt/static/media/har_beslutter.b6d6d8c5.svg"
+  },
+  {
+    "revision": "a2ef3c1aea3411853c5f379c313fb451",
+    "url": "/beslutteroversikt/static/media/klar_for_utsendelse.a2ef3c1a.svg"
+  },
+  {
+    "revision": "1fc576bfc1bdb617ef4de3982882dd2e",
+    "url": "/beslutteroversikt/static/media/trenger_beslutter.1fc576bf.svg"
+  },
+  {
+    "revision": "3adc36e470b1ae4705ab5238307af118",
+    "url": "/beslutteroversikt/static/media/trenger_tilbakemelding.3adc36e4.svg"
+  },
+  {
+    "revision": "ca9fdcb28ce11f5e32f4b9cccfde1460",
+    "url": "/beslutteroversikt/static/media/venter_pa_respons.ca9fdcb2.svg"
+  },
+  {
+    "revision": "7ac793016798be56f8e948bfa208ad2d",
+    "url": "/beslutteroversikt/static/media/x.7ac79301.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/beslutteroversikt/precache-manifest.50f7df2d922733935caa5c68afc260cf.js"
+  "/beslutteroversikt/precache-manifest.2e634299afe716a6f3fd6dfd494948ad.js"
 );
 
 self.addEventListener('message', (event) => {
