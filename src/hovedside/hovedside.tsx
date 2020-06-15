2825d815c55c925fd2164db5a39aed608dccd573
Sneha Desai <sneha.desai@nav.no>
2020-06-15 10:39:27 +0200

    KAIZEN-0: Fjernet ubrukt import fra hovedside

--- src/hovedside/hovedside.tsx
+++ src/hovedside/hovedside.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { DataFetcher } from '../components/datafetcher';
 import { UserTable } from '../components/user-table/user-table';
 import { Filters } from '../components/filters/filters';
 import { PaginationBar } from '../components/pagination-bar/pagination-bar';
