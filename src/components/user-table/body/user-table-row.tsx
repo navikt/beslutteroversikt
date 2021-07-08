dd8914e15e6fbf41ba266c43480580e766e0efe4
[Klara Margrethe Helgemo] <[klara.margrethe.helgemo@nav.no]>
2021-07-08 14:52:06 +0200

    TK-38 Forbedringer kvalitetssikring-14a. Har fjernet Filters, samt .less-filene for bruker-filter og status-dropdown. Styling lagt inn i filters.less. Tittel har nå samme venstre marg som tabellen. Feilretting som etterspurt i PR. 26.08: Styling ikoner plassert i .less-fil

--- src/components/user-table/body/user-table-row.tsx
+++ src/components/user-table/body/user-table-row.tsx
@@ -9,11 +9,17 @@ import { DialogDots, DialogReport } from '@navikt/ds-icons';
 import { AddPeople } from '@navikt/ds-icons';
 import './user-table-body.less';
 
-export const UserRow = (props: { idx: number, bruker: Bruker, aktivEnhet: OrNothing<string> }) => {
+export const UserRow = (props: { idx: number; bruker: Bruker; aktivEnhet: OrNothing<string> }) => {
 	const {
-		brukerFnr, brukerFornavn, brukerEtternavn, statusEndret,
-		brukerOppfolgingsenhetNavn, vedtakStartet,
-		beslutterNavn, veilederNavn, status
+		brukerFnr,
+		brukerFornavn,
+		brukerEtternavn,
+		statusEndret,
+		brukerOppfolgingsenhetNavn,
+		vedtakStartet,
+		beslutterNavn,
+		veilederNavn,
+		status
 	} = props.bruker;
 
 	const erMaskert = brukerFnr === '';
@@ -31,15 +37,32 @@ export const UserRow = (props: { idx: number, bruker: Bruker, aktivEnhet: OrNoth
 
 	return (
 		<div role="row" aria-rowindex={props.idx} className="user-table-row">
-			<a className={cls('user-table-row__innhold', {'user-table-row__innhold--maskert': erMaskert})} href={lagBrukerUrl()}>
-				<Normaltekst tag="span" role="cell" style={alignStart}>{lagBrukerNavn(brukerFornavn, brukerEtternavn)}</Normaltekst>
-				<Element tag="span" role="cell">{brukerFnr}</Element>
-				<Normaltekst tag="span" role="cell">{formatDateStr(vedtakStartet)}</Normaltekst>
-				<UtkastStatusData status={status}/>
-				<Element tag="span" role="cell" style={alignStart}>{beslutterNavn || '-'}</Element>
-				<Normaltekst tag="span" role="cell" style={alignStart}>{veilederNavn}</Normaltekst>
-				<Normaltekst tag="span" role="cell">{formatDateTime(statusEndret)}</Normaltekst>
-				<Normaltekst tag="span" role="cell">{fjernNavFraEnhetNavn(brukerOppfolgingsenhetNavn)}</Normaltekst>
+			<a
+				className={cls('user-table-row__innhold', { 'user-table-row__innhold--maskert': erMaskert })}
+				href={lagBrukerUrl()}
+			>
+				<Normaltekst tag="span" role="cell" style={alignStart}>
+					{lagBrukerNavn(brukerEtternavn, brukerFornavn)}
+				</Normaltekst>
+				<Element tag="span" role="cell">
+					{brukerFnr}
+				</Element>
+				<Normaltekst tag="span" role="cell">
+					{formatDateStr(vedtakStartet)}
+				</Normaltekst>
+				<UtkastStatusData status={status} />
+				<Element tag="span" role="cell" style={alignStart}>
+					{beslutterNavn || '-'}
+				</Element>
+				<Normaltekst tag="span" role="cell" style={alignStart}>
+					{veilederNavn}
+				</Normaltekst>
+				<Normaltekst tag="span" role="cell">
+					{formatDateTime(statusEndret)}
+				</Normaltekst>
+				<Normaltekst tag="span" role="cell">
+					{fjernNavFraEnhetNavn(brukerOppfolgingsenhetNavn)}
+				</Normaltekst>
 			</a>
 		</div>
 	);
@@ -53,29 +76,25 @@ const UtkastStatusData = (props: { status: UtkastStatus }) => {
 	switch (props.status) {
 		case UtkastStatus.TRENGER_BESLUTTER:
 			StatusIkon = AddPeople;
-			ikonFarge = ''
+			ikonFarge = 'status_ikon__default';
 			break;
 		case UtkastStatus.KLAR_TIL_BESLUTTER:
 			StatusIkon = DialogReport;
-			ikonFarge = 'status_ikon__oransje'
+			ikonFarge = 'status_ikon__oransje';
 			break;
 		case UtkastStatus.KLAR_TIL_VEILEDER:
 			StatusIkon = DialogDots;
-			ikonFarge = 'status_ikon__bla'
+			ikonFarge = 'status_ikon__bla';
 			break;
 		case UtkastStatus.GODKJENT_AV_BESLUTTER:
 			StatusIkon = DialogDots;
-			ikonFarge = 'status_ikon__bla'
+			ikonFarge = 'status_ikon__bla';
 			break;
 	}
 
 	return (
 		<span role="cell" className={'status'}>
-			<StatusIkon className={ikonFarge}
-			aria-label={ariaLabel}
-			role="img"
-			focusable="false"
-			/>
+			<StatusIkon className={`status_ikon ${ikonFarge}`} aria-label={ariaLabel} role="img" focusable="false" />
 			<Normaltekst>{mapBrukerStatusTilTekst(props.status)}</Normaltekst>
 		</span>
 	);
