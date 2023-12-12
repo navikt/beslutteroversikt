33b5e5af6d40fcb8e2335f89dc9f8f8af5b958a4
Mathias S. Müller <mathias.sundt.muller@nav.no>
2023-12-12 17:25:13 +0100

    Bytt komponenter i prelansering-side.tsx
    Co-authored-by: Klara M Helgemo <klara.margrethe.helgemo@nav.no>

--- src/prelansering-side/prelansering-side.tsx
+++ src/prelansering-side/prelansering-side.tsx
@@ -1,5 +1,5 @@
 import pcBilde from './prelansering_info_pc.svg';
-import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
+import { BodyShort, Heading } from '@navikt/ds-react';
 import './prelansering-side.less';
 
 export const PrelanseringInfoSide = () => {
@@ -7,16 +7,18 @@ export const PrelanseringInfoSide = () => {
 		<div className="prelansering-inneholder">
 			<div className="wrapper">
 				<div className="wrapper-inneholdt">
-					<Systemtittel>Ny oversikt for besluttere som kvalitetssikrer arbeidsevnevurderinger</Systemtittel>
-					<Normaltekst className="info-tekst">
+					<Heading size="medium" level="1">
+						Ny oversikt for besluttere som kvalitetssikrer arbeidsevnevurderinger
+					</Heading>
+					<BodyShort className="info-tekst">
 						I beslutteroversikten får du oversikt over alle enhetene du er beslutter for. Der får du også
 						oversikt over status på arbeidsevnevurderingene du er beslutter for.
-					</Normaltekst>
-					<Normaltekst className="info-tekst">
-						Når kommer den?
+					</BodyShort>
+					<BodyShort className="info-tekst">
+						<i>Når kommer den?</i>
 						<br />
 						Beslutteroversikten lanseres sammen med ny løsning for § 14a-vedtak i Modia.
-					</Normaltekst>
+					</BodyShort>
 				</div>
 				<div className="wrapper-inneholdt">
 					<img src={pcBilde} className="pcBilde" alt="Beslutteroversikt bilde" />
