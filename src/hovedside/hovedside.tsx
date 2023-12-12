a017be0ecaed2cc12ae44159641d681e733fe109
Mathias S. Müller <mathias.sundt.muller@nav.no>
2023-12-12 17:23:48 +0100

    Rydd i hovedside.tsx
    Co-authored-by: Klara M Helgemo <klara.margrethe.helgemo@nav.no>

--- src/hovedside/hovedside.tsx
+++ src/hovedside/hovedside.tsx
@@ -1,16 +1,14 @@
 import { Header } from '../components/header/header';
 import { PaginationBar } from '../components/pagination-bar/pagination-bar';
-import './hovedside.less';
-import { Header } from '../components/header/header';
+import { UserTable } from '../components/user-table/user-table';
+import './hovedside.css';
 
 export const Hovedside = () => {
 	return (
 		<main className="hovedside">
-			<div className="hovedside__table">
-				<Header />
-				<PaginationBar />
-				<UserTable />
-			</div>
+			<Header />
+			<PaginationBar />
+			<UserTable />
 		</main>
 	);
 };
