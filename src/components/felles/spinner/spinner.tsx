2cff1bd597408bab5ea217732dd79652b21db96c
Mathias S. Müller <mathias.sundt.muller@nav.no>
2023-12-15 15:12:26 +0100

    Endre Spinner-tittel til venter på data

--- src/components/felles/spinner/spinner.tsx
+++ src/components/felles/spinner/spinner.tsx
@@ -2,7 +2,7 @@ import { Loader } from '@navikt/ds-react';
 import './spinner.css';
 
 function Spinner() {
-	return <Loader size="2xlarge" title="venter..." className="vedtaksstotte-spinner" />;
+	return <Loader size="2xlarge" title="Venter på data" className="vedtaksstotte-spinner" />;
 }
 
 export default Spinner;
