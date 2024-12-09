1b279b6add84d109e46e4668348421c40434159f
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2024-12-09 18:45:30 +0100

    Skriv om til å bruke ESLint flat config + installer nødvendige tilleggsavhengigheiter

--- eslint.config.lintstaged.js
+++ eslint.config.lintstaged.js
@@ -1,24 +1,33 @@
-export default [
-	{
-		env: {
-			browser: true,
-			es2021: true,
-			node: true
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+
+export default tseslint.config({
+	languageOptions: {
+		globals: {
+			...globals.browser,
+			...globals.es2025
 		},
-		extends: [
-			'eslint:recommended',
-			'plugin:jsx-a11y/recommended',
-			'plugin:react/recommended',
-			'plugin:react-hooks/recommended',
-			'plugin:@typescript-eslint/recommended',
-			'prettier'
-		],
-		parser: '@typescript-eslint/parser',
-		parserOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module'
-		},
-		plugins: ['react', '@typescript-eslint'],
-		rules: { 'no-debugger': 'error', 'no-console': 'error' }
+		parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
+	},
+	extends: [eslint.configs.recommended, tseslint.configs.recommended],
+	files: ['**/*.{js,jsx,ts,tsx}'],
+	plugins: {
+		'jsx-a11y': jsxA11y,
+		'react-hooks': reactHooks,
+		'react-refresh': reactRefresh
+	},
+	rules: {
+		...jsxA11y.flatConfigs.recommended.rules,
+		...reactHooks.configs.recommended.rules,
+		'react-refresh/only-export-components': 'warn',
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/no-explicit-any': 'warn',
+		'@typescript-eslint/no-empty-object-type': 'off',
+		'no-debugger': 'error',
+		'no-console': 'error'
 	}
-];
+});
