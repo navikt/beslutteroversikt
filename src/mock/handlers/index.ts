c2bb834f0309052228e1803f0ea387b51518bb0f
Sondre Larsen Ovrid <111113539+slovrid@users.noreply.github.com>
2024-12-10 10:15:23 +0100

    Bumper avhengigheiter (#297)
    * Bump avhengigheiter
    
    * Rename frå "api" til "handlers" for å vere tettare på terminologien til msw
    
    * Skriv om til å bruke ESLint flat config + installer nødvendige tilleggsavhengigheiter
    
    * Ignorer en ESLint error til vi veit kva vi skal gjere med fila
    
    * Endre til å bruke eksakte versjonar av avhengigheitene
    
    * Legger til nokre paths med "passthrough" request-handlers for å unngå warnings i console

--- src/mock/handlers/index.ts
+++ src/mock/handlers/index.ts
@@ -2,9 +2,11 @@ import { RequestHandler } from 'msw';
 import { veilarbvedtaksstotteHandlers } from './veilarbvedtaksstotte';
 import { modiacontextholderHandlers } from './modiacontextholder';
 import { veilarbveilederHandlers } from './veilarbveileder';
+import { defaultHandlers } from './default';
 
 export const allHandlers: RequestHandler[] = [
 	...veilarbvedtaksstotteHandlers,
 	...modiacontextholderHandlers,
-	...veilarbveilederHandlers
+	...veilarbveilederHandlers,
+	...defaultHandlers
 ];
--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,4 +1,4 @@
 import { setupWorker } from 'msw/browser';
-import { allHandlers } from './api';
+import { allHandlers } from './handlers';
 
 export const worker = setupWorker(...allHandlers);
