62fc5eab740d201d72133eb4da8f886c0ed09963
Mathias S. Müller <mathias.sundt.muller@nav.no>
2025-02-10 09:53:03 +0100

    Fiks lint og fjern cpu-limit

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 
-export default tseslint.config({
+export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
 	languageOptions: {
 		globals: {
 			...globals.browser,
@@ -13,15 +14,17 @@ export default tseslint.config({
 		},
 		parserOptions: { ecmaVersion: 'latest', sourceType: 'module' }
 	},
-	extends: [eslint.configs.recommended, tseslint.configs.recommended],
 	files: ['**/*.{js,jsx,ts,tsx}'],
 	plugins: {
 		'jsx-a11y': jsxA11y,
+		react: reactPlugin,
 		'react-hooks': reactHooks,
 		'react-refresh': reactRefresh
 	},
 	rules: {
 		...jsxA11y.flatConfigs.recommended.rules,
+		...reactPlugin.configs.recommended.rules,
+		...reactPlugin.configs['jsx-runtime'].rules, // React >=17
 		...reactHooks.configs.recommended.rules,
 		'react-refresh/only-export-components': 'warn',
 		'@typescript-eslint/ban-ts-comment': 'off',
@@ -29,5 +32,10 @@ export default tseslint.config({
 		'@typescript-eslint/no-empty-object-type': 'off',
 		'no-debugger': 'warn',
 		'no-console': 'warn'
+	},
+	settings: {
+		react: {
+			version: 'detect' // reactPlugin trenger å vite dette
+		}
 	}
 });
