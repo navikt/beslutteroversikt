418afdd69e784b881d438b4b41ab59f3836ad9f2
Ingrid Fosså <ingrid.fossa@nav.no>
2025-02-14 11:50:38 +0100

    Trekk ut utkaststatus-komponenten til eiga fil

--- src/components/user-table/body/user-table-row.tsx
+++ src/components/user-table/body/user-table-row.tsx
@@ -1,17 +1,17 @@
 import { Bleed, BodyShort, CopyButton, Tooltip } from '@navikt/ds-react';
-import { ChatElipsisIcon, ChatExclamationmarkIcon, PersonPlusIcon } from '@navikt/aksel-icons';
-import { Bruker, UtkastStatus } from '../../../rest/data/bruker';
+import { Bruker } from '../../../rest/data/bruker';
 import { formatDateStr, formatDateStrWithMonthName, formatTimeStr } from '../../../utils/date-utils';
-import { capitalize, fjernNavFraEnhetNavn, lagBrukerNavn, mapBrukerStatusTilTekst } from '../../../utils';
+import { capitalize, fjernNavFraEnhetNavn, lagBrukerNavn } from '../../../utils';
 import { BrukerDirektelenkeMedFeilmelding } from '../bruker-direktelenke-med-feilmelding';
+import { UtkastStatusData } from './status/utkast-status-data';
 import './user-table-body.less';
 
-interface UserRowProps {
+interface Props {
 	idx: number;
 	bruker: Bruker;
 }
 
-export const UserRow = ({ idx, bruker }: UserRowProps) => {
+export const UserRow = ({ idx, bruker }: Props) => {
 	const {
 		brukerFnr,
 		brukerFornavn,
@@ -56,50 +56,5 @@ export const UserRow = ({ idx, bruker }: UserRowProps) => {
 				<BodyShort size="small">{fjernNavFraEnhetNavn(brukerOppfolgingsenhetNavn)}</BodyShort>
 			</div>
 		</div>
-	);
-};
-
-interface UtkastStatusDataProps {
-	status: UtkastStatus;
-}
-
-const UtkastStatusData = ({ status }: UtkastStatusDataProps) => {
-	let StatusIkon;
-	switch (status) {
-		case UtkastStatus.TRENGER_BESLUTTER:
-			StatusIkon = (
-				<Bleed marginBlock="1 2" asChild>
-					<PersonPlusIcon title="Trenger kvalitetssikrer-ikon" className="status_ikon" />
-				</Bleed>
-			);
-			break;
-		case UtkastStatus.KLAR_TIL_BESLUTTER:
-			StatusIkon = (
-				<Bleed marginBlock="0 3" asChild>
-					<ChatExclamationmarkIcon title="Venter på tilbakemelding-ikon" className="status_ikon" />
-				</Bleed>
-			);
-			break;
-		case UtkastStatus.KLAR_TIL_VEILEDER:
-			StatusIkon = (
-				<Bleed marginBlock="0 3" asChild>
-					<ChatElipsisIcon title="Venter på veileder-ikon" className="status_ikon" />
-				</Bleed>
-			);
-			break;
-		case UtkastStatus.GODKJENT_AV_BESLUTTER:
-			StatusIkon = (
-				<Bleed marginBlock="0 3" asChild>
-					<ChatElipsisIcon title="Godkjent av kvalitetssikrer-ikon" className="status_ikon" />
-				</Bleed>
-			);
-			break;
-	}
-
-	return (
-		<span role="cell" className={'status'}>
-			{StatusIkon}
-			<BodyShort size="small">{mapBrukerStatusTilTekst(status)}</BodyShort>
-		</span>
 	);
 };
