1b592df90507fa715d99355a6ffc77539286bd58
Ingrid Fosså <ingrid.fossa@nav.no>
2025-02-21 15:30:40 +0100

    Legg tekst og ikon for utkaststatus i eit felles objekt så det blir lettare å sjå kva tekst som høyrer saman med kva ikon

--- src/components/user-table/body/status/utkast-status-data.tsx
+++ src/components/user-table/body/status/utkast-status-data.tsx
@@ -1,50 +1,51 @@
 import { Bleed, BodyShort } from '@navikt/ds-react';
 import { ChatElipsisIcon, ChatExclamationmarkIcon, PersonPlusIcon } from '@navikt/aksel-icons';
 import { UtkastStatus } from '../../../../rest/data/bruker';
-import { mapBrukerStatusTilTekst } from '../../../../utils';
 
 interface Props {
 	status: UtkastStatus;
 }
 
 export const UtkastStatusData = ({ status }: Props) => {
-	let StatusIkon;
-
-	switch (status) {
-		case UtkastStatus.TRENGER_BESLUTTER:
-			StatusIkon = (
-				<Bleed marginBlock="1 2" asChild>
-					<PersonPlusIcon title="Trenger kvalitetssikrer-ikon" className="status_ikon" />
-				</Bleed>
-			);
-			break;
-		case UtkastStatus.KLAR_TIL_BESLUTTER:
-			StatusIkon = (
-				<Bleed marginBlock="0 3" asChild>
-					<ChatExclamationmarkIcon title="Venter på tilbakemelding-ikon" className="status_ikon" />
-				</Bleed>
-			);
-			break;
-		case UtkastStatus.KLAR_TIL_VEILEDER:
-			StatusIkon = (
-				<Bleed marginBlock="0 3" asChild>
-					<ChatElipsisIcon title="Venter på veileder-ikon" className="status_ikon" />
-				</Bleed>
-			);
-			break;
-		case UtkastStatus.GODKJENT_AV_BESLUTTER:
-			StatusIkon = (
-				<Bleed marginBlock="0 3" asChild>
-					<ChatElipsisIcon title="Godkjent av kvalitetssikrer-ikon" className="status_ikon" />
-				</Bleed>
-			);
-			break;
-	}
-
 	return (
-		<span role="cell" className={'status'}>
-			{StatusIkon}
-			<BodyShort size="small">{mapBrukerStatusTilTekst(status)}</BodyShort>
+		<span role="cell" className="status">
+			{utkaststatusTekstOgIkon[status].ikon}
+			<BodyShort size="small">{utkaststatusTekstOgIkon[status].tekst}</BodyShort>
 		</span>
 	);
+};
+
+const utkaststatusTekstOgIkon: { [key in UtkastStatus]: { tekst: string; ikon: React.ReactNode } } = {
+	[UtkastStatus.TRENGER_BESLUTTER]: {
+		tekst: 'Trenger kvalitetssikring',
+		ikon: (
+			<Bleed marginBlock="1 2" asChild>
+				<PersonPlusIcon title="Trenger kvalitetssikrer-ikon" className="status_ikon" />
+			</Bleed>
+		)
+	},
+	[UtkastStatus.KLAR_TIL_BESLUTTER]: {
+		tekst: 'Venter på tilbakemelding',
+		ikon: (
+			<Bleed marginBlock="0 3" asChild>
+				<ChatExclamationmarkIcon title="Venter på tilbakemelding-ikon" className="status_ikon" />
+			</Bleed>
+		)
+	},
+	[UtkastStatus.KLAR_TIL_VEILEDER]: {
+		tekst: 'Venter på veileder',
+		ikon: (
+			<Bleed marginBlock="0 3" asChild>
+				<ChatElipsisIcon title="Venter på veileder-ikon" className="status_ikon" />
+			</Bleed>
+		)
+	},
+	[UtkastStatus.GODKJENT_AV_BESLUTTER]: {
+		tekst: 'Klar til utsendelse',
+		ikon: (
+			<Bleed marginBlock="0 3" asChild>
+				<ChatElipsisIcon title="Godkjent av kvalitetssikrer-ikon" className="status_ikon" />
+			</Bleed>
+		)
+	}
 };
