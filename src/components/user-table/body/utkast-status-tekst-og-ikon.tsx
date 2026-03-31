1183d600bd5fc0a34949c5ff1e4aa37679a24cb8
Sanne Edland <sanneed@hotmail.com>
2026-03-31 10:50:07 +0200

    Fix: spacing på ikoner

--- src/components/user-table/body/utkast-status-tekst-og-ikon.tsx
+++ src/components/user-table/body/utkast-status-tekst-og-ikon.tsx
@@ -7,7 +7,7 @@ export const utkaststatusTekstOgIkon: { [key in UtkastStatus]: { tekst: string; 
 	[UtkastStatus.TRENGER_BESLUTTER]: {
 		tekst: 'Trenger kvalitetssikring',
 		ikon: (
-			<Bleed marginBlock="space-1" asChild>
+			<Bleed marginBlock="space-4 space-8" asChild>
 				<PersonPlusIcon aria-hidden={true} className="status_ikon" />
 			</Bleed>
 		)
@@ -15,7 +15,7 @@ export const utkaststatusTekstOgIkon: { [key in UtkastStatus]: { tekst: string; 
 	[UtkastStatus.KLAR_TIL_BESLUTTER]: {
 		tekst: 'Venter på tilbakemelding',
 		ikon: (
-			<Bleed marginBlock="space-1" asChild>
+			<Bleed marginBlock="space-0 space-12" asChild>
 				<ChatExclamationmarkIcon aria-hidden={true} className="status_ikon" />
 			</Bleed>
 		)
@@ -23,7 +23,7 @@ export const utkaststatusTekstOgIkon: { [key in UtkastStatus]: { tekst: string; 
 	[UtkastStatus.KLAR_TIL_VEILEDER]: {
 		tekst: 'Venter på veileder',
 		ikon: (
-			<Bleed marginBlock="space-1" asChild>
+			<Bleed marginBlock="space-0 space-12" asChild>
 				<ChatElipsisIcon aria-hidden={true} className="status_ikon" />
 			</Bleed>
 		)
@@ -31,7 +31,7 @@ export const utkaststatusTekstOgIkon: { [key in UtkastStatus]: { tekst: string; 
 	[UtkastStatus.GODKJENT_AV_BESLUTTER]: {
 		tekst: 'Klar til utsendelse',
 		ikon: (
-			<Bleed marginBlock="space-1" asChild>
+			<Bleed marginBlock="space-0 space-12" asChild>
 				<ChatElipsisIcon aria-hidden={true} className="status_ikon" />
 			</Bleed>
 		)
